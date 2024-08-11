5c13e54ff3df84ffca8074475746fd152a239659
Brian Bechtel <brianrbechtel@gmail.com>
2024-08-11 11:24:59 -0700

    add FlatMap test

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -60,3 +60,76 @@ func TestMap(t *testing.T) {
 		})
 	}
 }
+
+func TestFlatMap(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name        string
+		input       []int
+		mappingFunc func(int) chan string
+		want        []string
+	}{
+		{
+			name:  "map_empty",
+			input: []int{},
+			mappingFunc: func(i int) chan string {
+				t.Error("mapping function was called when it should not have been")
+				return nil
+			},
+			want: nil,
+		},
+		{
+			name:  "map_one",
+			input: []int{1},
+			mappingFunc: func(n int) chan string {
+				c := make(chan string)
+				go func() {
+					for i := 0; i <= n; i++ {
+						c <- strconv.Itoa(i)
+					}
+					close(c)
+				}()
+				return c
+			},
+			want: []string{"0", "1"},
+		},
+		{
+			name:  "map_many",
+			input: []int{1, 2, 3},
+			mappingFunc: func(n int) chan string {
+				c := make(chan string)
+				go func() {
+					for i := 0; i <= n; i++ {
+						c <- strconv.Itoa(i)
+					}
+					close(c)
+				}()
+				return c
+			},
+			want: []string{"0", "1", "0", "1", "2", "0", "1", "2", "3"},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			input := FromSlice(tc.input)
+			mappedChan := FlatMap(input, tc.mappingFunc)
+			got := ToSlice(mappedChan)
+			if diff := cmp.Diff(got, tc.want); diff != "" {
+				t.Errorf("unexpected result (-got, +want): %s", diff)
+			}
+			// check that both channels are closed now
+			_, ok := <-input
+			if ok {
+				t.Error("expected input to be closed ")
+			}
+			_, ok = <-mappedChan
+			if ok {
+				t.Error("expected mappedChan to be closed ")
+			}
+		})
+	}
+}
